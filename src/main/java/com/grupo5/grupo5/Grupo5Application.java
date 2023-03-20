af41122ef25f5e935ae55b02995e403c03adc0a6
LAPTOP-UOADHCKT\Pedro <pedroankersmituni@gmail.com>
2023-03-20 13:37:59 +0100

    Subida del proyecto inicial

---
+++ src/main/java/com/grupo5/grupo5/Grupo5Application.java
@@ -0,0 +1,14 @@
+package com.grupo5.grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class Grupo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo5Application.class, args);
+    }
+
+}
