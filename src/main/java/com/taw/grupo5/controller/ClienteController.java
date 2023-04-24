942b2377f4a0d9dfa2a257c6e237018703d4381a
ignamarga1 <0619967319@uma.es>
2023-04-24 19:28:01 +0200

    refactorizado y arreglado

--- src/main/java/com/taw/grupo5/controller/ClienteController.java
+++ src/main/java/com/taw/grupo5/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.taw.grupo5.controller;
 import javax.servlet.http.HttpSession;
 
 import com.taw.grupo5.dao.ClienteRepository;
+import com.taw.grupo5.entity.ClienteEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
