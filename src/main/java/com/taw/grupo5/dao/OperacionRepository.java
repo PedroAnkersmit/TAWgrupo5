6cedbf20515a40bd5f7dcd17d526fdbfae55c382
ignamarga1 <0619967319@uma.es>
2023-04-30 13:46:38 +0200

    query para buscar operaciones por idCliente

--- src/main/java/com/taw/grupo5/dao/OperacionRepository.java
+++ src/main/java/com/taw/grupo5/dao/OperacionRepository.java
@@ -2,6 +2,12 @@ package com.taw.grupo5.dao;
 
 import com.taw.grupo5.entity.OperacionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OperacionRepository extends JpaRepository<OperacionEntity, Integer> {
+    @Query("select o from OperacionEntity o where o.idcliente = :idCliente")
+    List<OperacionEntity> buscarPorCliente(@Param("idCliente") Integer idCliente);
 }
