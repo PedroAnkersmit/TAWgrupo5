109205901b13c0e3e10dd67481b8fc16283c045d
ignamarga1 <0619967319@uma.es>
2023-04-30 13:47:04 +0200

    añadido operacionRepository

--- src/main/java/com/taw/grupo5/controller/GestorController.java
+++ src/main/java/com/taw/grupo5/controller/GestorController.java
@@ -2,8 +2,10 @@ package com.taw.grupo5.controller;
 
 import com.taw.grupo5.dao.ClienteRepository;
 import com.taw.grupo5.dao.EmpresaRepository;
+import com.taw.grupo5.dao.OperacionRepository;
 import com.taw.grupo5.entity.ClienteEntity;
 import com.taw.grupo5.entity.EmpresaEntity;
+import com.taw.grupo5.entity.OperacionEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +24,9 @@ public class GestorController {
     @Autowired
     EmpresaRepository empresaRepository;
 
+    @Autowired
+    OperacionRepository operacionRepository;
+
     @GetMapping("/")
     public String mostrarClientesYEmpresas(Model model) {
         List<ClienteEntity> clienteEntityList = this.clienteRepository.findAll();
@@ -37,6 +42,9 @@ public class GestorController {
     public String mostrarDatosCliente(@RequestParam("id") Integer idCliente, Model model) {
         ClienteEntity clienteEntity = this.clienteRepository.findById(idCliente).orElse(null);
         model.addAttribute("cliente", clienteEntity);
+
+        List<OperacionEntity> operacionRepositoryList = this.operacionRepository.buscarPorCliente(idCliente);
+        model.addAttribute("listaOperaciones", operacionRepositoryList);
 
         return "gestorCliente";
     }
