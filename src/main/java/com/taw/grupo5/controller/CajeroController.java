d9741f1ca9990161dc0b1d6a7ac281983070e134
PablosGon <100775130+PablosGonUMA@users.noreply.github.com>
2023-04-30 09:45:08 +0200

    Agregado método login

--- src/main/java/com/taw/grupo5/controller/CajeroController.java
+++ src/main/java/com/taw/grupo5/controller/CajeroController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -28,6 +30,12 @@ public class CajeroController {
         return "cajeroDatos";
     }
 
+    @PostMapping("/cajero/login")
+    public String doLogin(Model model, @ModelAttribute("cliente") ClienteEntity cliente){
 
+
+
+        return "cajeroLogin";
+    }
 
 }
