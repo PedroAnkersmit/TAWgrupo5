eda052c89d6acf4e7421addc2fced3106bebf26b
ignamarga1 <0619967319@uma.es>
2023-04-30 12:33:58 +0200

    creación de OperacionRepository

---
+++ src/main/java/com/taw/grupo5/dao/OperacionRepository.java
@@ -0,0 +1,7 @@
+package com.taw.grupo5.dao;
+
+import com.taw.grupo5.entity.OperacionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperacionRepository extends JpaRepository<OperacionEntity, Integer> {
+}
