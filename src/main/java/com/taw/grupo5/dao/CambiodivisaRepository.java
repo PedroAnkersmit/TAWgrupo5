52b64bbe5fa4ba3717cb382e26a8f3cb9a137199
Jesús Ariza <0619966879@uma.es>
2023-05-04 17:14:29 +0200

    -

--- src/main/java/com/taw/grupo5/dao/CambiodivisaRepository.java
+++ src/main/java/com/taw/grupo5/dao/CambiodivisaRepository.java
@@ -1,7 +1,17 @@
-package com.taw.grupo5.dao;
+package com.taw.grupo5.dao;/*
+Created by Pedro Ankersmit Carrión
+*/
 
 import com.taw.grupo5.entity.CambiodivisaEntity;
+import com.taw.grupo5.entity.OperacionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CambiodivisaRepository extends JpaRepository<CambiodivisaEntity, Integer> {
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface CambioDivisaRepository extends JpaRepository<CambiodivisaEntity, Integer> {
+    @Query("select c.operacionByIdoperacion from CambiodivisaEntity c where c.operacionByIdoperacion.idcliente = :id")
+    List<OperacionEntity> BuscarPorCliente(@Param("id") Integer idCliente);
 }
