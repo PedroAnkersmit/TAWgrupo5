0f6687d67d0e7b3637ebf374666baebfe2625567
ignamarga1 <0619967319@uma.es>
2023-05-04 10:52:08 +0200

    listado clientes pertenecientes a empresa

--- src/main/java/com/taw/grupo5/dao/ClienteRepository.java
+++ src/main/java/com/taw/grupo5/dao/ClienteRepository.java
@@ -12,4 +12,7 @@ public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer>
     @Query("select c from ClienteEntity  c where c.nombre like CONCAT('%', :filtro, '%') or c.email like CONCAT('%', :filtro, '%')")
     List<ClienteEntity> buscarPorNombre (@Param("filtro") String filtro);
 
+    @Query("select c from ClienteEntity c where c.empresaByIdempresa.idempresa = :filtro")
+    List<ClienteEntity> buscarPorEmpresa(@Param("filtro") Integer filtro);
+
 }
