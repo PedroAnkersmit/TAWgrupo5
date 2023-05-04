079b1e89af0561f9cd9761445b89ad89d90d9af7
Jesús Ariza <0619966879@uma.es>
2023-05-04 18:13:38 +0200

    Prueba

--- src/main/java/com/taw/grupo5/controller/EmpresaController.java
+++ src/main/java/com/taw/grupo5/controller/EmpresaController.java
@@ -181,7 +181,10 @@ public class EmpresaController {
 
         for(OperacionEntity ope : listaOperaciones)
         {
-            if(filtro.getFechaMinima() <= ope.getFecha()
+            if(filtro.getFechaMinima() <= ope.getFecha())
+            {
+
+            }
         }
 
         model.addAttribute("clientePortal", cliente);
